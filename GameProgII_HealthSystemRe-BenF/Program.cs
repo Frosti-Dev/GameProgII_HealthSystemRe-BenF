825ba7376c31384255cb616f1b23a6f45109ac00
Frosti-Dev <bennyfranken3@gmail.com>
2026-01-09 12:14:12 -0400

    base

---
+++ GameProgII_HealthSystemRe-BenF/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProgII_HealthSystemRe_BenF
+{
+    class Program
+    {
+        class Health
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
